c530b46d9d884777a2e2e996b8299fcb15302b85
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2023-04-28 22:18:08 +0300

    1_пятница конец

--- src/features/counter/counterSlice.ts
+++ src/features/counter/counterSlice.ts
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { RootState, AppThunk } from "../../app/store";
+import { RootState, AppThunk } from "app/store";
 import { fetchCount } from "./counterAPI";
 
 export interface CounterState {
@@ -12,11 +12,6 @@ const initialState: CounterState = {
   status: "idle",
 };
 
-// The function below is called a thunk and allows us to perform async logic. It
-// can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
-// will call the thunk with the `dispatch` function as the first argument. Async
-// code can then be executed and other actions can be dispatched. Thunks are
-// typically used to make async requests.
 export const incrementAsync = createAsyncThunk("counter/fetchCount", async (amount: number) => {
   const response = await fetchCount(amount);
   // The value we return becomes the `fulfilled` action payload
@@ -29,16 +24,11 @@ export const counterSlice = createSlice({
   // The `reducers` field lets us define reducers and generate associated actions
   reducers: {
     increment: (state) => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
       state.value += 1;
     },
     decrement: (state) => {
       state.value -= 1;
     },
-    // Use the PayloadAction type to declare the contents of `action.payload`
     incrementByAmount: (state, action: PayloadAction<number>) => {
       state.value += action.payload;
     },
